8a67dc3772ed8f86519fc32c1acbc184f72cc404
ismail ayhan <139040563+isometax@users.noreply.github.com>
2025-11-27 21:33:20 +0100

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-27T20:33:20.880Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-27T20:33:23.253Z
+Commit: Fix CSS styling bug on mobile devices
