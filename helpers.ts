9505e079964690cbde4e64cd7f63b01dd7ef6ce2
ismail ayhan <139040563+isometax@users.noreply.github.com>
2025-11-27 21:25:26 +0100

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-27T20:24:49.316Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-27T20:25:28.771Z
+Commit: Add export functionality for reports
